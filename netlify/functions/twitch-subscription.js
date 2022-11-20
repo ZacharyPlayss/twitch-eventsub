a22ab0f0df28a6d51649a4b77b411eb3d343a254
Zachary Van De Staey <zacharyplays2.0@gmail.com>
2022-11-20 21:35:18 +0100

    feat: show subs!

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,12 +1,27 @@
 exports.handler = async (event) => {
-    const { headers = []} = event; 
+    const { headers = [] } = event; 
+    
+    const type = headers['twitch-eventsub-message-type'] || 'no type';
+    const eventType = headers['twitch-eventsub-subscription-type'];
 
-    const type = headers['twitch-eventsub-message-type'] ?? 'no type';
+
+
+    if (type !== 'notification' || eventType !== 'channel.subscribe') {
+        return { statusCode: 200, body: 'ok'};
+    }
+
+    console.log({ headers: event.headers, body: event.body });
+    
+    //at this point you know you have a valid subscription
+    //want to do some magic? Put it here!
+    const { event: twitchEvent } = JSON.parse(event.body);
+    const user = twitchEvent.user_name;
+
     return {
         statusCode: 200,
         body: JSON.stringify({
             type,
-            message: 'ok',
+            message: `${user} just subscribed!`,
         }),
     };
 };
