fb4c7599bc0b88c5d21efa91d9844b026eb37d58
Zachary Van De Staey <75170539+ZacharyPlayss@users.noreply.github.com>
2022-11-20 21:53:43 +0100

    Update twitch-subscription.js

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,27 +1,24 @@
 exports.handler = async (event) => {
-    const { headers = [] } = event; 
-    
-    const type = headers['twitch-eventsub-message-type'] || 'no type';
-    const eventType = headers['twitch-eventsub-subscription-type'];
+  const { headers = [] } = event;
 
+  const type = headers['twitch-eventsub-message-type'] || 'no type';
+  const eventType = headers['twitch-eventsub-subscription-type'];
 
+  if (type !== 'notification' || eventType !== 'channel.subscribe') {
+    return { statusCode: 200, body: '' };
+  }
 
-    if (type !== 'notification' || eventType !== 'channel.subscribe') {
-        return { statusCode: 200, body: 'ok'};
-    }
+  console.log({ headers: event.headers, body: event.body });
 
-    console.log({ headers: event.headers, body: event.body });
-    
-    //at this point you know you have a valid subscription
-    //want to do some magic? Put it here!
-    const { event: twitchEvent } = JSON.parse(event.body);
-    const user = twitchEvent.user_name;
+  // at this point you know you have a valid subscription
+  // want to do some magic? put it here!
+  const { event: twitchEvent } = JSON.parse(event.body);
+  const user = twitchEvent.user_name;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            type,
-            message: `${user} just subscribed!`,
-        }),
-    };
+  console.log(`${user} just subscribed!`);
+
+  return {
+    statusCode: 200,
+    body: '',
+  };
 };
